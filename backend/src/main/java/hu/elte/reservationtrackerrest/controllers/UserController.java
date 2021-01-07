2012f9b99f4076b6cb86d5f602b62740dfba0a87
Don3y <dhorvath0818@gmail.com>
2021-01-07 18:20:00 +0100

    Cros Enabled

--- backend/src/main/java/hu/elte/reservationtrackerrest/controllers/UserController.java
+++ backend/src/main/java/hu/elte/reservationtrackerrest/controllers/UserController.java
@@ -9,9 +9,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/users")
 public class UserController {
     
